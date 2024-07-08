ce01a0866278765b0ee5e18bd61b566c8a737b18
Dron245 <Dron245@yandex.ru>
2024-07-08 08:14:05 +0300

    0807-0814

--- src/js/react/Components/photos/App.jsx
+++ src/js/react/Components/photos/App.jsx
@@ -3,35 +3,47 @@ import "./index.scss";
 import Collection from "./components/Collection.jsx";
 import axios from "axios";
 
+const cats = [
+	"Все", "Горы", "Море", "Архитектура", "Города"
+]
 function App() {
 	const [collections, setCollections] = useState([]);
 	const [searchValue, setSearchValue] = useState("");
+	const [categories, setcategories] = useState(0);
+	const [isLoading, setIsLoading] = useState(true);
+	const [page, setPage] = useState(1)
 	useEffect(() => {
 		async function fethdata() {
 			try {
-				const res = await axios.get("https://66853f80b3f57b06dd4bf714.mockapi.io/photos");
+				const categ = categories ? `&category=${categories}` : ''
+				setIsLoading(true)
+				const res = await axios.get(`https://66853f80b3f57b06dd4bf714.mockapi.io/photos?p=${page}&l=2${categ}&`);
 				setCollections(res.data);
+				setIsLoading(false)
 			} catch (error) {
 				console.log(error);
 				console.log("не получилось");
 			}
 		}
 		fethdata();
-	}, []);
+	}, [categories, page]);
 	return (
 		<div className="App">
 			<h1>Моя коллекция фотографий</h1>
 			<div className="top">
 				<ul className="tags">
-					<li className="active">Все</li>
-					<li>Горы</li>
-					<li>Море</li>
-					<li>Архитектура</li>
-					<li>Города</li>
+					{cats.map((item, index)=> (
+						<li 
+						onClick={()=> setcategories(index)} 
+						className={categories === index ? 'active' : ''} 
+						key={index} >{item}
+						</li>
+					))}
 				</ul>
 				<input value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="search-input" placeholder="Поиск по названию" />
 			</div>
-			<div className="content">
+			{isLoading ? <h3>Загрузка...</h3> : (
+				<div className="content">
 				{collections.filter(collection=>collection.name.toLowerCase().includes(searchValue.toLowerCase()))
 				.map((collection, index) => {
 					return <Collection 
@@ -40,10 +52,21 @@ function App() {
 					key={index} />;
 				})}
 			</div>
+			)}
+			
 			<ul className="pagination">
-				<li>1</li>
+				{[...Array(5)].map((it, i)=> 
+				<li 
+				key={i}
+				onClick={()=> setPage(i+1)}
+				className={page === i+1 ? 'active' : ''} 
+				>
+					{i+1}
+				</li>
+				)}
+				{/* <li>1</li>
 				<li className="active">2</li>
-				<li>3</li>
+				<li>3</li> */}
 			</ul>
 		</div>
 	);
