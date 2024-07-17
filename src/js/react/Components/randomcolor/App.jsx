e88e63c449a8a73a26af1fef4fde0099645a582b
Dron245 <Dron245@yandex.ru>
2024-07-17 22:01:34 +0300

    1707-2201

--- src/js/react/Components/randomcolor/App.jsx
+++ src/js/react/Components/randomcolor/App.jsx
@@ -1,9 +1,51 @@
-import React, {useState, useEffect} from 'react'
-
+import React, { useState, useEffect } from "react";
+import "./index.scss";
 const App = () => {
-  return (
-	 <></>
-  )
-}
+	const [type, setType] = useState(null);
+	const [color, setcolor] = useState("#000000");
+	console.log(type)
+	console.log(color);
+	const generate = () => {
+		if (type==='hex') {
+			const hex = ['A', 'B','C','D','E','F']
+			let hexResult = '#'
+			for (let i = 0; i < 6; i++) {
+				let element = hex[ Math.floor(Math.random()*hex.length)];
+				console.log(element);
+				hexResult +=element
+			}
+			setcolor(hexResult)
+		} else if (type==='rgb') {
+			const r = Math.floor(Math.random()*256)
+			const g = Math.floor(Math.random()*256)
+			const b = Math.floor(Math.random()*256)
+			setcolor(`#${r}${g}${b}`)
+		}
+	}
 
-export default App
+	const changeHex = () => {
+		setType('hex')
+	}
+	const changeRgb = () => {
+		setType('rgb')
+	}
+	useEffect(() => {
+		generate()
+	
+	 
+	}, [type])
+	
+	
+	return (
+		<div style={{ background: color }} className="wrapper-control">
+				<h3>{type && type==='hex' ? `hex ${color}` : type==='rgb' ? `rgb ${color}` : null}</h3>
+			<div className="control">
+				<button onClick={changeHex}>HEX</button>
+				<button onClick={changeRgb}>RGB</button>
+				<button onClick={generate}>сгенерировать</button>
+			</div>
+		</div>
+	);
+};
+
+export default App;
